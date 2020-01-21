ce9797d1b0652b3fe8a30056955ade5c85addbac
Wayne Clingingsmith <wayne@wind2walk.com>
2020-01-20 20:44:04 -0800

    Slider

--- src/components/SlideView/index.js
+++ src/components/SlideView/index.js
@@ -5,11 +5,11 @@ import "../../styles/slideview.css";
 
 const Wrapper = styled.div`
     width: 70%;
-    margin: 0 auto;
 `;
 
 const Page = styled.div`
     align:center;
+    margin: 10 auto;
 `;
 
 const Text = styled.div`
@@ -34,10 +34,11 @@ const settings = {
 //export default class SlideView {content, className} extends React.Component {
 export const SlideView = ({content, className}) => (
 
-    <Wrapper>
+    <Wrapper className="grey lighten-4">
         <Slider {...settings}>
             {content.map((image, index) => (
                 <Page key={index}>
+                    <br/>
                     <img src={image.imageSrc}  alt={image.imagealt}/>
                     <br/>
                     <Text>{image.imageText}</Text>
